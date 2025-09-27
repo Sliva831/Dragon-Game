32585858f36827ccdae26b986804d3652f3dbfa9
Sliva831 <slavazgen@gmail.com>
2025-09-27 16:24:53 +0300

    PlayerController

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(horiz * moveSpeed, rb.velocity.y);
+
+    }
+}
