b07a417feb5128abdacc0e1f9d66a0d246bdfedd
Sliva831 <slavazgen@gmail.com>
2025-10-12 16:50:28 +0300

    Health 2

---
+++ Assets/heart/Health.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float startingHealth;
+    public float currentHealth;
+    private Animator anim;
+    private bool dead = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage,0, startingHealth);
+
+        if(currentHealth > 0)
+        {
+            // Анимация по получению урона 
+        }
+        if(currentHealth <= 0)
+        {
+            // Анимация смерти
+            dead = true;
+            GetComponent <PlayerController>().enabled = false;
+        }
+
+    }
+
+    public void AddHealth(float _value)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+}
---
+++ Assets/heart/HealthBar.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeathBar : MonoBehaviour
+{
+    public Health playerHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
