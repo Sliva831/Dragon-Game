dcf47ea131d50da769f5b8d41e22d7dd61ec49e0
Sliva831 <slavazgen@gmail.com>
2025-10-18 13:10:50 +0300

    Health 3

--- Assets/heart/HealthBar.cs
+++ Assets/heart/HealthBar.cs
@@ -4,16 +4,18 @@ using UnityEngine.UI;
 public class HeathBar : MonoBehaviour
 {
     public Health playerHealth;
+    public Image totalHeart;
+    public Image currentHeart;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        totalHeart.fillAmount = playerHealth.currentHealth / 10;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        currentHeart.fillAmount = playerHealth.currentHealth / 10;
     }
 }
